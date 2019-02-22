847398e596b48e62b2af7592323c542dc5820c1e
Gaëtan CESARO <egcesaro@cafat.nc>
2019-02-22 15:30:14 +1100

    Exploring the Angular Template Syntax

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,13 +1,34 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-    selector: 'event-thumbnail',
-    templateUrl: './event-thumbnail.component.html',
-    styles: [`
+  selector: 'event-thumbnail',
+  templateUrl: './event-thumbnail.component.html',
+  styles: [`
       .pad-left { margin-left: 50px; }
       .well div { color: #bbb; }
+      .green { color: #003300 !important; }
+      .bold { font-weight: bold; }
     `]
 })
 export class EventThumbnailComponent {
-    @Input() event: any
+  @Input() event: any;
+
+  hideDetails: boolean;
+
+  hideShowDetails() {
+    this.hideDetails = !this.hideDetails;
+    return this.hideDetails;
+  }
+
+  getStartTimeClass() {
+    if (this.event && this.event.time === '8:00 am')
+      return ['green', 'bold'];
+    return [];
+  }
+
+  getStartTimeStyle(): any {
+    if (this.event && this.event.time === '8:00 am')
+      return { color: '#003300', 'font-weight': 'bold' }
+    return {}
+  }
 }
