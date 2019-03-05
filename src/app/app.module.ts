33b2079e1b7dd554b6878d29f2e70fd7b8a7bda5
Gaëtan CESARO <egcesaro@cafat.nc>
2019-03-05 17:28:17 +1100

    Organizing export / imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ToastrService } from './common/toastr.service';
+
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventService,
+  EventRouteActivator,
+  EventListResolver
+} from './events/index'
 
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
-import { NavBarComponent } from './nav/navbar.component';
-import { EventService } from './events/shared/event.service';
-import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { RouterModule } from '@angular/router';
 import { appRoutes } from 'src/routes';
-import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventListResolver } from './events/event-list-resolver.service';
+import { NavBarComponent } from './nav/navbar.component';
+
 
 @NgModule({
   imports: [
@@ -49,3 +53,4 @@ export function checkDirtyState(component: CreateEventComponent) {
   }
   return true;
 }
+
