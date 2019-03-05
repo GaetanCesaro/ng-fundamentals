c01d7fb76da59ed0be593741417cf7a53394909f
Gaëtan CESARO <egcesaro@cafat.nc>
2019-03-05 17:20:31 +1100

    New User Module

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import { CreateEventComponent } from './app/events/create-event.component';
 import { Error404Component } from './app/errors/404.component';
 import { EventRouteActivator } from './app/events/event-details/event-route-activator.service';
 import { EventListResolver } from './app/events/event-list-resolver.service';
+import { userRoutes } from './app/user/user.routes';
 
 export const appRoutes: Routes = [
     { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
@@ -12,4 +13,5 @@ export const appRoutes: Routes = [
     { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
     { path: '404', component: Error404Component },
     { path: '', redirectTo: '/events', pathMatch: 'full' },
+    { path: 'user', loadChildren: './user/user.module#UserModule' }
 ]
