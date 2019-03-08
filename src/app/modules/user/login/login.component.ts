88b3c403f05369fe2eb035738698f5b9f5bb26dd
Gaëtan CESARO <gaetan.cesaro@gmail.com>
2019-03-08 20:11:53 +1100

    Validating template-based forms

--- src/app/modules/user/login/login.component.ts
+++ src/app/modules/user/login/login.component.ts
@@ -1,23 +1,34 @@
-import { Component } from '@angular/core';
-import { AuthService } from '../../../services/auth.service';
-import { Router } from '@angular/router';
+import { Component } from "@angular/core";
+import { AuthService } from "../../../services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html'
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styles: [
+    `
+      em {
+        float: right;
+        color: #e05c65;
+        padding-left: 10px;
+      }
+    `
+  ]
 })
 export class LoginComponent {
-  constructor(private authService: AuthService, private router: Router) {
+  userName;
+  password;
+  mouseoverLogin;
 
-  }
+  constructor(private authService: AuthService, private router: Router) {}
 
-  login(formValues) {
+  login(loginForm) {
+    let formValues = loginForm.value;
     this.authService.loginUser(formValues.userName, formValues.password);
-    this.router.navigate(['/events']);
+    this.router.navigate(["/events"]);
   }
 
   cancel() {
-    this.router.navigate(['/events']);
+    this.router.navigate(["/events"]);
   }
-
 }
