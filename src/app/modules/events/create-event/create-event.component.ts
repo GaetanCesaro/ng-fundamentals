d7d154d894df296a60d17f3f03c38b3ac742469f
Gaëtan CESARO <egcesaro@cafat.nc>
2019-03-12 16:46:58 +1100

    Add new event

--- src/app/modules/events/create-event/create-event.component.ts
+++ src/app/modules/events/create-event/create-event.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
+import { IEvent } from 'src/app/shared/models';
+import { EventService } from 'src/app/services/event.service';
 
 @Component({
-    selector: 'create-event',
-    templateUrl: './create-event.component.html',
-    styles: [
-      `
+  selector: 'create-event',
+  templateUrl: './create-event.component.html',
+  styles: [
+    `
         em {
           float: right;
           color: #e05c65;
@@ -27,22 +29,44 @@ import { Router } from '@angular/router';
         .error :ms-input-placeholder {
           color: #999;
         }
+        .event-image { height: 100px; }
       `
-    ]
+  ]
 })
-export class CreateEventComponent {
-  newEvent
+export class CreateEventComponent implements OnInit {
+  newEvent: any
   isDirty: boolean = false;
 
-    constructor(private location: Location, private router: Router) {
+  constructor(private location: Location, private router: Router, private eventService: EventService) {
+  }
+
+  ngOnInit() {
+    this.newEvent = {
+      name: 'Pot de départ !',
+      date: '04/15/2019',
+      time: '5pm',
+      price: 0,
+      location: {
+        address: 'Le Fronton',
+        city: 'Nouméa',
+        country: 'Nouvelle Calédonie'
+      },
+      onlineUrl: 'https://www.facebook.com/frontonetchekhan/',
+      imageUrl: 'https://www.lebonsiropderable.com/wp-content/uploads/2015/07/biere-quebec_Molson-Export_le-bon-sirop-d-erable.jpg'
     }
 
-    saveEvent(formValues) {
-      console.log(formValues);
-    }
+    console.log(this.newEvent)
+  }
 
-    cancel() {
-        //this.location.back();
-        this.router.navigate(['/events']);
-    }
+  saveEvent(formValues: IEvent) {
+    console.log("saveEvent: ", formValues);
+    this.isDirty = false;
+    this.eventService.saveEvent(formValues);
+    this.router.navigate(['/events']);
+  }
+
+  cancel() {
+    //this.location.back();
+    this.router.navigate(['/events']);
+  }
 }
