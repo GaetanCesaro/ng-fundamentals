b5402bd473c7c8220e34a95a6329c5cefa3a99a2
Gaetan CESARO <gaetan.cesaro@gmail.com>
2019-05-20 15:11:20 +1100

    Sessions search with API

--- src/app/shared/nav/navbar.component.ts
+++ src/app/shared/nav/navbar.component.ts
@@ -1,28 +1,33 @@
-import { Component } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
-import { ISession } from '../models/event.model';
-import { EventService } from '../../modules/events';
+import { Component } from "@angular/core";
+import { AuthService } from "../../services/auth.service";
+import { ISession } from "../models/event.model";
+import { EventService } from "../../modules/events";
 
 @Component({
-    selector: 'nav-bar',
-    templateUrl: './navbar.component.html',
-    styles: [`
+  selector: "nav-bar",
+  templateUrl: "./navbar.component.html",
+  styles: [
+    `
         .nav.navbar-nav { font-size: 15px; }
         .navbar-form { padding: 0px; }
         @media (max-width: 1200px) { #searchForm {display} }
         li > a.active { color: #F97924; }
         .search-input { max-width: 150px;}
-    `]
+    `
+  ]
 })
 export class NavBarComponent {
-    searchTerm: string = "";
-    foundSessions: ISession[];
+  searchTerm: string = "";
+  foundSessions: ISession[];
 
-    constructor(private authService: AuthService, private eventService: EventService) {
+  constructor(
+    private authService: AuthService,
+    private eventService: EventService
+  ) {}
 
-    }
-
-    searchSessions(searchTerm: string) {
-        this.eventService.searchSessions(searchTerm).subscribe(sessions => { this.foundSessions = sessions; })
-    }
+  searchSessions(searchTerm: string) {
+    this.eventService.searchSessions(searchTerm).subscribe(sessions => {
+      this.foundSessions = sessions;
+    });
+  }
 }
