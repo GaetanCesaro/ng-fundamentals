c9059c20c2052501084c3d4cdd6089ac0b03fd5c
Gaëtan CESARO <gaetan.cesaro@gmail.com>
2019-06-17 10:19:41 +1100

    Login with backend

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from "@angular/core";
 import { IUser } from "../shared/models/user.model";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { of } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root"
@@ -7,13 +10,20 @@ import { IUser } from "../shared/models/user.model";
 export class AuthService {
   currentUser: IUser;
 
+  constructor(private http: HttpClient) {}
+
   loginUser(userName: string, password: string) {
-    this.currentUser = {
-      id: 1,
-      userName: userName,
-      firstName: "John",
-      lastName: "Papa"
-    };
+    let options = { headers: new HttpHeaders({ "Content-Type": "application/json" }) };
+    let loginInfo = { username: userName, password: password };
+
+    return this.http.post('/api/login', loginInfo, options)
+      .pipe(tap(data => {
+        this.currentUser = <IUser>data['user'];
+      }))
+      .pipe(catchError(err => {
+        return of(false)
+      }));
+    ;
   }
 
   isAuthenticated() {
