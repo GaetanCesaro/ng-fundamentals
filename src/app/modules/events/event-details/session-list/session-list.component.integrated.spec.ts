424afb56237a0892ac260ec930bf079ebc5f2236
Gaëtan CESARO <gaetan.cesaro@gmail.com>
2019-06-17 18:09:50 +1100

    Integration tests

---
+++ src/app/modules/events/event-details/session-list/session-list.component.integrated.spec.ts
@@ -0,0 +1,55 @@
+import { SessionListComponent } from './session-list.compopent';
+import { ISession, CollapsibleWellComponent } from '../../../../shared';
+
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { AuthService } from 'src/app/services/auth.service';
+import { DurationPipe } from 'src/app/shared/pipes/duration.pipe';
+
+describe('SessionListComponent', () => {
+  let fixture: ComponentFixture<SessionListComponent>,
+   component : SessionListComponent,
+   element: HTMLElement,
+   debulEl: DebugElement;
+
+  beforeEach(async(() => {
+    let mockAuthService = {
+      isAuthenticated: () => true
+    };
+
+    TestBed.configureTestingModule({
+      imports: [],
+      declarations: [ SessionListComponent, DurationPipe, CollapsibleWellComponent ],
+      providers: [
+        { provide: AuthService, useValue: mockAuthService }
+      ],
+      schemas: []
+    });
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SessionListComponent);
+    component = fixture.componentInstance;
+    element = fixture.nativeElement;
+    debulEl = fixture.debugElement;
+  });
+
+  describe('Initial Display', () => {
+    it('should have the correct title', () => {
+      component.sessions = <ISession[]>[
+        {id: 2, name: 'Session 2', level: 'intermediate', duration: 1, presenter: 'John', abstract: 'abstract', voters: []},
+        {id: 1, name: 'Session 1', level: 'intermediate', duration: 2, presenter: 'Jean', abstract: 'abstract',voters: []}
+      ];
+
+      component.filterBy = 'all';
+      component.sortBy = 'name';
+
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expect(element.querySelector('[well-title]').textContent).toContain('Session 1');
+      expect(debulEl.query(By.css('[well-title]')).nativeElement.textContent).toContain('Session 1');
+    });
+  })
+})
--- src/app/modules/events/event-details/session-list/session-list.component.isolated.spec.ts
+++ src/app/modules/events/event-details/session-list/session-list.component.isolated.spec.ts
@@ -23,8 +23,33 @@ describe('SessionListComponent', () => {
       component.ngOnChanges();
 
       expect(component.filteredSessions.length).toBe(2);
+
+
+      component.filterBy = 'all';
+
+      component.ngOnChanges();
+
+      expect(component.filteredSessions.length).toBe(4);
+    });
+
+    it('should sort sessions correctly', () => {
+      component.sessions = <ISession[]>[
+        {name: 'Session 2', level: 'intermediate'},
+        {name: 'Session 1', level: 'intermediate'},
+        {name: 'Session 3', level: 'beginner'},
+        {name: 'Session 4', level: 'advanced'}
+      ];
+
+      component.filterBy = 'all';
+      component.sortBy = 'name';
+
+      component.ngOnChanges();
+
       expect(component.filteredSessions[0].name).toBe('Session 1');
       expect(component.filteredSessions[1].name).toBe('Session 2');
+      expect(component.filteredSessions[2].name).toBe('Session 3');
+      expect(component.filteredSessions[3].name).toBe('Session 4');
+
     });
   })
 })
